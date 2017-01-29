b2d8d03ac397dec9d860f57890bd17a999c3e36a
Zeick <timo.j.karkkainen@helsinki.fi>
2017-01-29 21:52:48 +0200

    Tokan viikon tehtävät lisätty nyt GitHubiin.

--- ratebeer/app/models/brewery.rb
+++ ratebeer/app/models/brewery.rb
@@ -1,3 +1,35 @@
 class Brewery < ActiveRecord::Base
-  has_many :beers
+  has_many :beers, dependent: :destroy
+  has_many :ratings, through: :beers
+
+  def print_report
+    puts self.name
+    puts "established at year #{self.year}"
+    puts "number of beers #{self.beers.count}"
+  end
+
+  def average_rating
+    all_ratings = 0
+    all_ratings_score = 0
+    self.beers.each do |beer|
+      all_ratings += beer.ratings.count
+      if beer.ratings.empty?
+      
+      else
+        beer.ratings.each do |rat|
+          all_ratings_score += rat.score 
+        end
+      end
+    end 
+    if all_ratings == 0
+
+    else # to_f varmistaa että saadaan liukuluku, round(1) pyöristää 1 desimaalin tarkkuuteen
+          return (all_ratings_score/all_ratings.to_f).round(1)
+    end
+  end
+  
+  def restart
+    self.year = 2017
+    puts "changed year to #{year}"
+  end
 end
