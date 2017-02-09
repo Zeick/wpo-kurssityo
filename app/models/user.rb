0b0d10bd2eda7c96c2875d27fb7c9c150109fe10
Zeick <timo.j.karkkainen@helsinki.fi>
2017-02-09 19:10:30 +0200

    Lisätty varoitusteksti virheelliselle salasanalle

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,9 +2,10 @@ class User < ActiveRecord::Base
 	include RatingAverage
 	has_secure_password
 	validates :username, uniqueness: true,
-						 length: { minimum: 3, maximum: 30 }	# Käyttäjätunnuksen on oltava vähintään 3 merkkiä ja yksikäsitteinen
+			     length: { minimum: 3, maximum: 30 }	# Käyttäjätunnuksen on oltava vähintään 3 merkkiä ja yksikäsitteinen
 	validates :password, length: { minimum: 4 }
-	validates_format_of :password, :with => /[A-Z]+.*[0-9]+|[0-9]+.*[A-Z]+/ # RegEx: On oltava ainakin yksi iso kirjain ja numero
+	validates :password, format: { with: /([A-Z].*\d)|(\d.*[A-Z].*)/,
+              message: "should contain at least one number and one capital letter." } 
 	has_many :ratings, dependent: :destroy # Käyttäjällä voi olla useita arvosteluja. Arvostelut poistetaan, jos käyttäjä poistetaan
 	has_many :beers, through: :ratings
 	has_many :memberships, dependent: :destroy
