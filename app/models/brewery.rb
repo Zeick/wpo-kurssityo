aa96b9ecb0ecf2bfbd5951250b33e8074c53ddb3
Zeick <timo.j.karkkainen@helsinki.fi>
2017-02-09 19:03:34 +0200

    Maksimivuosiluku automaattiseksi :)

--- app/models/brewery.rb
+++ app/models/brewery.rb
@@ -4,7 +4,7 @@ class Brewery < ActiveRecord::Base
   has_many :ratings, through: :beers
   validates :name, length: { minimum: 1 }
   validates :year, numericality: { greater_than_or_equal_to: 1042,
-                                   less_than_or_equal_to: 2017,
+                                   less_than_or_equal_to: Proc.new { Time.now.year },
                                    only_integer: true }
 
   def print_report
