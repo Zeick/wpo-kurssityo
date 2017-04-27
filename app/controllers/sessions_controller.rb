3054043fcb01d44ad0de65ff49368e07a7805b5b
Zeick <timo.j.karkkainen@helsinki.fi>
2017-04-27 21:40:03 +0300

    Korjattu sisäänkirjautumisessa ollut bugi

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -6,16 +6,18 @@ class SessionsController < ApplicationController
 	def create
 		# Hakee käyttäjätunnusta vastaavan käyttäjän tietokannasta
 		user = User.find_by username: params[:username]
-		if user && user.authenticate(params[:password]) && !user.banned
-			 # Jos käyttäjä on olemassa ja salasana täsmää...
-			session[:user_id] = user.id # ...tallennetaan sessioon kirjautuneen käyttäjän id ja...
-			redirect_to user, notice: "Welcome back!" # ...uudelleenohjataan käyttäjä omalle sivulleen
-		elsif user.banned # Bannittu käyttäjä ei pääse kirjautumaan
-			redirect_back(fallback_location: signin_path, notice: "Account #{params[:username]} has been banned. Please contact the administrator.")
+		if user
+			if user.authenticate(params[:password]) && !user.banned
+				 # Jos käyttäjä on olemassa ja salasana täsmää...
+				session[:user_id] = user.id # ...tallennetaan sessioon kirjautuneen käyttäjän id ja...
+				redirect_to user, notice: "Welcome back, #{params[:username]}!" # ...uudelleenohjataan käyttäjä omalle sivulleen
+			elsif user.banned # Bannittu käyttäjä ei pääse kirjautumaan
+				redirect_back(fallback_location: signin_path, notice: "Account #{params[:username]} has been banned. 🤔 Please contact the administrator.")
+			end
 		else
 #			redirect_to :back, notice: "Username #{params[:username]} and/or password mismatch!"
 			redirect_back(fallback_location: signin_path, notice: "Username #{params[:username]} and/or password mismatch!")
-		end
+		end		
 	end
 
 	def destroy
